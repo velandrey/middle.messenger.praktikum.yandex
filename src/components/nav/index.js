3c754e1c2e35c7cc156e831b03b975f735b674de
Andrey Velikanov <velandrey@yandex.ru>
2025-08-29 13:14:45 +1000

    Layout view

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,2 +1,8 @@
 import './style.pcss';
-export { default as Nav } from './template.hbs?raw';
+export function Nav({pages}) {
+    let html = '';
+    pages.forEach((page)=>{
+        html += `<a href="#" class="nav__link ${page.class}" data-link="${page.link}">${page.title}</a>`;
+    });
+    return html;
+}
